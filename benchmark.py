cc2af42d316e3ce8bf59808fbbc8d47f6db5665d
Jared Wasinger <j-wasinger@hotmail.com>
2022-12-26 01:10:47 -0500

    add missing script

---
+++ benchmark.py
@@ -0,0 +1,14 @@
+import os, subprocess, re
+
+# TODO pass cargo-path to this script
+cmd ="/home/jared/.cargo/bin/cargo bench -- G1Proj"
+result = subprocess.run(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if result.returncode != 0:
+    raise Exception("geth exec error: {}".format(result.stderr))
+res = re.search(r'^.*time:.*\[(.*)\]\\n', str(result.stdout)).groups()[0]
+res = res.split(' ')[0:2]
+if res[1] != '\\xc2\\xb5s':
+    raise Exception("unit should be microseconds")
+
+rust_exec_time = float(res[0])
+import pdb; pdb.set_trace()
